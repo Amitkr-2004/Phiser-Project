42bddbb40bfb850d8e3ec6bf014bc3c9e70430a4
Amit Kumar <amitkumar990374@gmail.com>
2024-09-30 17:37:09 +0530

    establishing setup.py

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Phishing-Classifier
 ML-Project-1
+
+# To show in git that the commits are done by us we have use set.py and write command in terminal->>
+
+ "  pip install -e .  " to run setup.py
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Amitkr',
+    author_email='amitkumar990374@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
